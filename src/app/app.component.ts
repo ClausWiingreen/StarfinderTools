48875abe10a9675b4760d9dede87488193721a04
Claus Wiingreen <claus@wiingreen.dk>
2022-02-17 16:40:21 +0100

    Changed the top bar to a side bar and enabled routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
-import { IconProp } from '@fortawesome/fontawesome-svg-core'
+import { IconProp } from '@fortawesome/fontawesome-svg-core';
+import { Router, Route, ParamMap } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -10,4 +11,17 @@ import { IconProp } from '@fortawesome/fontawesome-svg-core'
 export class AppComponent {
   title = 'Starfinder Tools';
   faGithub = faGithub as IconProp;
+
+  constructor(public router: Router) {
+  }
+
+  nameLookup(route: Route) : string {
+    const path = route.path;
+    if(!path) return "undefined";
+    switch(path){
+      case "background": return "Character Background Generator";
+      case "character": return "Character Generator";
+    }
+    return path;
+  }
 }
